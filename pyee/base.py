c0d5ae95802df15d0d8fe03b7d3e78d8731ab0e6
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-10-10 08:26:15 +0900

    fix

--- pyee/base.py
+++ pyee/base.py
@@ -20,7 +20,7 @@ class PyeeException(Exception):
     """An exception internal to pyee."""
 
 
-Handler = TypeVar(name="Handler", bound=[Callable])
+Handler = TypeVar(name="Handler", (), bound=Callable)
 
 
 class EventEmitter:
